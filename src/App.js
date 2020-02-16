9ea15f8ce3a9272802f873872b9281c32275095b
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-02-16 01:24:15 -0500

    Create react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import classes from "./App.module.css";
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <header className={classes.App_header}>
+        <img src={logo} className={classes.App_logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
