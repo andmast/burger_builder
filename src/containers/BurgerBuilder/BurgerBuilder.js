82245b9623b712d5dc4dcff0a2d62bc5228117e6
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-02-17 10:57:30 -0500

    Displaying and Updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,6 +67,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabledInfo={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
